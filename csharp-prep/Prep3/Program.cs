7b8ff31b2491f70acc69fdbfb7f0c97e6f340629
Straton11 <stratonfacer1@gmail.com>
2023-01-05 15:04:49 -0700

    Prep1-5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomnum = new Random();
+        int correctnum = randomnum.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != correctnum)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (correctnum > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (correctnum < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }  
     }
 }
