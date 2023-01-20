6f7783cb71145ddf73c8c96883b1ce6427294534
Straton11 <stratonfacer1@gmail.com>
2023-01-20 15:23:44 -0700

    Scripture Memorizer

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Reference scriptureReference = new Reference("Proverbs", "3", "5-6");
+        Scripture scripture = new Scripture(scriptureReference, "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths.");
+        scriptureMemorizer scriptureMemorizer = new scriptureMemorizer(scripture);
+
+        string userInput = " ";
+
+        do
+        {
+            Console.Clear();
+            Console.WriteLine(string.Format("{0} {1}", scriptureReference.toString(), scriptureMemorizer.toString()));
+            Console.WriteLine("Please enter to continue or type 'quit' to finish:");
+            userInput = Console.ReadLine();
+            scriptureMemorizer.removeWordsFromText();
+        }
+        while (userInput != "quit" && scriptureMemorizer.hasWordsLeft() == true);
     }
+    
 }
---
+++ prove/Develop03/ScriptureMemorizer.cs
@@ -0,0 +1,54 @@
+using System;
+
+class scriptureMemorizer
+{
+    private Scripture scripture;
+    private List<string> scriptureTextList;
+
+    public scriptureMemorizer(Scripture _scripture)
+    {
+        scripture = _scripture;
+        scriptureTextList = new List<string>();
+        convertTextToList();
+    }
+
+    private void convertTextToList()
+    {
+        scriptureTextList = scripture.toString().Split(' ').ToList();
+    }
+
+    public void removeWordsFromText()
+    {
+        int numWordsToRemove = new Random().Next(3, 4);
+        int wordsRemoved = 0;
+
+        do
+        {
+            int rndIndex = new Random().Next(0, scriptureTextList.Count());
+            if (scriptureTextList[rndIndex].Contains('_') == false)
+            {   scriptureTextList[rndIndex] = new string('_', scriptureTextList[rndIndex].Length);
+                wordsRemoved++;
+            }
+        } while (wordsRemoved != numWordsToRemove);
+        
+    }
+
+    public string toString()
+    {
+        return string.Join(' ', scriptureTextList);
+    }
+    bool retvalue = false;
+    public bool hasWordsLeft()
+    {
+        foreach (string word in scriptureTextList)
+        {
+            if (word.Contains('_') == false)
+            {
+                retvalue = true;
+                break;
+            }
+        }
+
+        return retvalue;
+    }
+}
