e16de32ab16a3da995d534e6331f7e53cbd4b8a2
Straton11 <stratonfacer1@gmail.com>
2023-01-24 11:34:47 -0700

    Learning03

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -3,7 +3,7 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
+    {   
         Reference scriptureReference = new Reference("Proverbs", "3", "5-6");
         Scripture scripture = new Scripture(scriptureReference, "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths.");
         scriptureMemorizer scriptureMemorizer = new scriptureMemorizer(scripture);
